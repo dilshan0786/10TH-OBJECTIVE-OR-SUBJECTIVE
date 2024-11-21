9721d69c2581e677c5df7e80cf44a17e81459fbc
dilshan0786 <139648348+dilshan0786@users.noreply.github.com>
2024-11-22 00:29:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -69,6 +69,9 @@ function displayQuestions(questions, containerId) {
     });
 
     document.getElementById('score').style.display = 'block';
+
+    // Re-render MathJax
+  MathJax.typesetPromise();
 }
 
 function handleAnswer(selected, correct, button, questionElement) {
